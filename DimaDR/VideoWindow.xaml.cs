f289bb59c5fe65a88a8f17c75b35519916905861
student <student@101-03>
2018-10-01 18:56:17 +0300

    qqqq

---
+++ DimaDR/VideoWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DimaDR
+{
+    /// <summary>
+    /// Interaction logic for VideoWindow.xaml
+    /// </summary>
+    public partial class VideoWindow : Window
+    {
+        public VideoWindow()
+        {
+            InitializeComponent();
+        }
+
+        public VideoWindow(string path)
+        {
+            InitializeComponent();
+            ME_Player.Source = new Uri(path);
+        }
+
+        private void MediaElement_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ME_Player_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
